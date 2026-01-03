1bd0f657b7e190182b11cbd8a2ed4d42ac02becb
lksbldms <lukasbaldamus@googlemail.com>
2026-01-03 23:43:00 +0100

    setup architecture

---
+++ src/main/kotlin/de/bazi/kompro_backend/KomproBackendApplication.kt
@@ -0,0 +1,11 @@
+package de.bazi.kompro_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KomproBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<KomproBackendApplication>(*args)
+}
---
+++ src/test/kotlin/de/bazi/kompro_backend/KomproBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.bazi.kompro_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KomproBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
